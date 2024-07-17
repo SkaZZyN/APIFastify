69a6a40d72448c1c61914b43d9d6f70e5b79984e
Neto Santos <hunaldoneto23@gmail.com>
2024-07-16 21:21:09 -0300

    first commit

---
+++ database-postgres.js
@@ -0,0 +1,30 @@
+import { randomUUID } from "crypto";
+import { sql } from "./db.js";
+
+export class DatabasePostgres {
+  async list(search) {
+    let videos;
+
+    if (search) {
+      videos = await sql`select * from videos where title ilike ${
+        "%" + search + "%"
+      }`;
+    } else {
+      videos = await sql`select * from videos`;
+    }
+
+    return videos;
+  }
+  async create(video) {
+    const id = randomUUID();
+    await sql`insert into videos (id, title, description, duration) values (${id}, ${video.title}, ${video.description}, ${video.duration})`;
+  }
+
+  async update(id, video) {
+    await sql`update videos set title = ${video.title}, description = ${video.description}, duration = ${video.duration} where id = ${id}`;
+  }
+
+  async delete(id) {
+    await sql`delete from videos where id = ${id}`;
+  }
+}
